0a10a8f3dbb1d8135a7dc85a2844757b9e99393a
e.chernyak <e.chernyak@rbkmoney.com>
2021-06-03 11:48:33 +0300

    Template generated

---
+++ src/main/java/com/rbkmoney/scheduledpayoutworker/ScheduledPayoutWorkerApplication.java
@@ -0,0 +1,15 @@
+package com.rbkmoney.scheduledpayoutworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class ScheduledPayoutWorkerApplication extends SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledPayoutWorkerApplication.class, args);
+    }
+
+}
