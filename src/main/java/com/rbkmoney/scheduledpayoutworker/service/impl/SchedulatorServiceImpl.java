355eb29663674201dd599b0b773607cf7ecb8bbb
e.chernyak <e.chernyak@rbkmoney.com>
2021-06-16 11:58:03 +0300

    WIP. Comments after discussion added

--- src/main/java/com/rbkmoney/scheduledpayoutworker/service/impl/SchedulatorServiceImpl.java
+++ src/main/java/com/rbkmoney/scheduledpayoutworker/service/impl/SchedulatorServiceImpl.java
@@ -52,12 +52,12 @@ public class SchedulatorServiceImpl implements SchedulatorService {
         CalendarRef calendarRef = paymentInstitution.getCalendar();
         long revision = partyManagementService.getPartyRevision(partyId);
         shopMetaDao.save(partyId, shopId, calendarRef.getId(), scheduleRef.getId());
-
+        //deregister old schedule
         Schedule schedule = new Schedule();
         DominantBasedSchedule dominantBasedSchedule = new DominantBasedSchedule()
                 .setBusinessScheduleRef(new BusinessScheduleRef().setId(scheduleRef.getId()))
-                .setCalendarRef(calendarRef)
-                .setRevision(revision);
+                .setCalendarRef(calendarRef);
+                //.setRevision(revision); //TODO: Убрать
         schedule.setDominantSchedule(dominantBasedSchedule);
         RegisterJobRequest registerJobRequest = new RegisterJobRequest()
                 .setSchedule(schedule)
@@ -65,7 +65,7 @@ public class SchedulatorServiceImpl implements SchedulatorService {
                 .setContext(new byte[0]);
         //TODO: Откуда брать scheduleId / нужно ли вызывать deregister, если запись в БД уже есть?
         try {
-            schedulatorClient.registerJob(String.valueOf(scheduleRef.getId()), registerJobRequest);
+            schedulatorClient.registerJob(payouter-String.valueOf(scheduleRef.getId()), registerJobRequest);
         } catch (TException e) {
             //TODO: Обработка ScheduleAlreadyExists (вероятно на уровне вставки в shopMetaDao)
             throw new IllegalStateException(String.format("Register job '%s' failed", scheduleRef.getId()), e);
